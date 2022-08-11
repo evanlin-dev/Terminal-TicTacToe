35aff4609a919b14f48f77d7ee10732b3f133ceb
Blubaeri <evanlin2912@gmail.com>
2022-08-11 17:24:04 -0400

    Fixed accessing tttt

--- backend/tttt/urls.py
+++ backend/tttt/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import messages, message_edit
 
 urlpatterns = [
-    path('/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('messages/', messages, name='messages'),
     path('messages/<int:pk>/', message_edit, name='message_edit'),
 ]
